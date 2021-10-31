8a2badb2002fa0aac0b76b83b628a5fe2e3c966e
최진호 <lldaybreakll@gmail.com>
2021-10-31 15:54:07 +0900

    로그인 성공시 - 메인화면에서 내 정보 불러오기 API 호출  - 저장된 토큰값 활용 예시

--- app/src/main/java/com/neppplus/colosseum_20211024/LoginActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/LoginActivity.kt
@@ -29,8 +29,8 @@ class LoginActivity : BaseActivity() {
 
 //          isChecked : 클릭되어서 변경된 최종 상태값을 알려준다.
 //            Toast.makeText(mContext, isChecked.toString(), Toast.LENGTH_SHORT).show()
-//          ContextUtil에 변경된 값 저장
 
+//          ContextUtil에 변경된 값 저장
             ContextUtil.setAutoLogin(mContext, isChecked)
         }
 
